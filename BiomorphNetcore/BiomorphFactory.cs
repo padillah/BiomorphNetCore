1b4a193b3ac0ca51a79a9fa060236de4cf473fe1
Tom Padilla <padilla.henry@gmail.com>
2021-12-17 14:11:36 -0600

    Initial version of Biomorph

---
+++ BiomorphNetcore/BiomorphFactory.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BiomorphNetcore
+{
+    public static class BiomorphFactory
+    {
+        /** The total number of genes that make up a biomorph. */
+        public const int GENE_COUNT = 9;
+        /** The minimum permitted value for most genes. */
+        public const int GENE_MIN = -5;
+        /** The maximum permitted value for most genes. */
+        public const int GENE_MAX = 5;
+        /** The index of the gene that controls biomporph size. */
+        public const int LENGTH_GENE_INDEX = 8;
+        /** The minimum permitted value for the length gene. */
+        public const int LENGTH_GENE_MIN = 1;
+        /** The maximum permitted value for the length gene. */
+        public const int LENGTH_GENE_MAX = 7;
+
+        public static Biomorph generateRandomCandidate()
+        {
+            Random rand = new();
+            int[] genes = new int[GENE_COUNT];
+
+            for (int i = 0; i < GENE_COUNT - 1; i++)
+            {
+                // First 8 genes have values between -5 and 5.
+                genes[i] = rand.Next(11) - 5;
+            }
+            // Last genes ha a value between 1 and 7.
+            genes[LENGTH_GENE_INDEX] = rand.Next(LENGTH_GENE_MAX) + 1;
+
+            return new Biomorph(genes);
+        }
+
+        public static List<Biomorph> MutatePopulation(Biomorph rootPopulation)
+        {
+            List<Biomorph> mutatedPopulation = new List<Biomorph>(GENE_COUNT);
+            int mutatedGene = 0;
+            int mutation = 1;
+            for (int i = 0; i < GENE_COUNT; i++)
+            {
+                int[] genes = rootPopulation.GetGenotype();
+
+                mutation *= -1; // Alternate between incrementing and decrementing.
+                if (mutation == 1) // After gene has been both incremented and decremented, move to next one.
+                {
+                    mutatedGene = (mutatedGene + 1) % GENE_COUNT;
+                }
+
+                genes[mutatedGene] += mutation;
+                int min = mutatedGene == LENGTH_GENE_INDEX ? LENGTH_GENE_MIN : GENE_MIN;
+                int max = mutatedGene == LENGTH_GENE_INDEX ? LENGTH_GENE_MAX : GENE_MAX;
+                if (genes[mutatedGene] > max)
+                {
+                    genes[mutatedGene] = min;
+                }
+                else if (genes[mutatedGene] < min)
+                {
+                    genes[mutatedGene] = max;
+                }
+
+                mutatedPopulation.Add(new Biomorph(genes));
+            }
+
+            return mutatedPopulation;
+
+        }
+    }
+}
---
+++ BiomorphNetcore/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BiomorphNetcore
+{
+    public partial class Form1 : Form
+    {
+        List<BiomorphImage> pictureBoxes;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBoxes = new List<BiomorphImage>(9);
+
+            // Need a new Biomorph
+            Biomorph rootBiomorph = BiomorphFactory.generateRandomCandidate();
+
+            List<Biomorph> biomes = new List<Biomorph>();
+
+
+            for (int i = 0; i < BiomorphFactory.GENE_COUNT; i++)
+            {
+                BiomorphImage newImage = new BiomorphImage()
+                { BackColor = Color.AliceBlue, ForeColor = Color.Black, Dock = DockStyle.Fill };
+                newImage.Click += NewImage_Click;
+
+                pictureBoxes.Add(newImage);
+                biomes.Add(rootBiomorph.Clone());
+            }
+
+            tableLayoutPanel1.Controls.Add(pictureBoxes[0], 0, 0);
+            tableLayoutPanel1.Controls.Add(pictureBoxes[1], 1, 0);
+            tableLayoutPanel1.Controls.Add(pictureBoxes[2], 2, 0);
+            tableLayoutPanel1.Controls.Add(pictureBoxes[3], 0, 1);
+            tableLayoutPanel1.Controls.Add(pictureBoxes[4], 1, 1);
+            tableLayoutPanel1.Controls.Add(pictureBoxes[5], 2, 1);
+            tableLayoutPanel1.Controls.Add(pictureBoxes[6], 0, 2);
+            tableLayoutPanel1.Controls.Add(pictureBoxes[7], 1, 2);
+            tableLayoutPanel1.Controls.Add(pictureBoxes[8], 2, 2);
+
+            biomes = BiomorphFactory.MutatePopulation(biomes);
+
+            for (int index = 0; index < pictureBoxes.Count; index++)
+            {
+                pictureBoxes[index].draw(biomes[index]);
+            }
+        }
+
+        private void NewImage_Click(object? sender, EventArgs e)
+        {
+            BiomorphImage? currentImage = sender as BiomorphImage;
+            if (currentImage != null)
+            {
+                List<Biomorph> biomes = new List<Biomorph>();
+                var currentBiomorph = currentImage.Biomorph;
+
+                for (int i = 0; i < BiomorphFactory.GENE_COUNT; i++)
+                {
+                    biomes.Add(currentBiomorph.Clone());
+                }
+
+                biomes = BiomorphFactory.MutatePopulation(biomes);
+
+                for (int index = 0; index < pictureBoxes.Count; index++)
+                {
+                    pictureBoxes[index].draw(biomes[index]);
+                }
+            }
+        }
+    }
+}
