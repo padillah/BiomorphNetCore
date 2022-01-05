b0f8809039b94154955f60690407417e9eba9e2d
Tom Padilla <padilla.henry@gmail.com>
2022-01-05 11:11:28 -0600

    Initial working version

---
+++ WeaselProgram/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace WeaselProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPhrase = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.listBoxResults = new System.Windows.Forms.ListBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelRunTime = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxPhrase, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonStart, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.labelRunTime, 1, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(309, 556);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Target phrase:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBoxPhrase
+            // 
+            this.textBoxPhrase.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxPhrase.Location = new System.Drawing.Point(89, 3);
+            this.textBoxPhrase.Name = "textBoxPhrase";
+            this.textBoxPhrase.Size = new System.Drawing.Size(217, 23);
+            this.textBoxPhrase.TabIndex = 1;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(89, 37);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // listBoxResults
+            // 
+            this.listBoxResults.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxResults.FormattingEnabled = true;
+            this.listBoxResults.ItemHeight = 15;
+            this.listBoxResults.Location = new System.Drawing.Point(0, 0);
+            this.listBoxResults.Name = "listBoxResults";
+            this.listBoxResults.Size = new System.Drawing.Size(614, 556);
+            this.listBoxResults.TabIndex = 4;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Cursor = System.Windows.Forms.Cursors.VSplit;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listBoxResults);
+            this.splitContainer1.Size = new System.Drawing.Size(927, 556);
+            this.splitContainer1.SplitterDistance = 309;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Run Time:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelRunTime
+            // 
+            this.labelRunTime.AutoSize = true;
+            this.labelRunTime.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelRunTime.Location = new System.Drawing.Point(89, 68);
+            this.labelRunTime.Name = "labelRunTime";
+            this.labelRunTime.Size = new System.Drawing.Size(217, 15);
+            this.labelRunTime.TabIndex = 5;
+            this.labelRunTime.Text = "label3";
+            this.labelRunTime.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 556);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPhrase;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ListBox listBoxResults;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelRunTime;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
---
+++ WeaselProgram/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+// Add a switch to disregard phrase length
+// Add capability to include all ASCII characters
+
+namespace WeaselProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            Stopwatch runTime;
+
+            if (string.IsNullOrEmpty(textBoxPhrase.Text))
+            {
+                _ = MessageBox.Show("The phrase is blank. There is nothing to compute.\r\nPlease enter a phrase and start again.");
+                _ = textBoxPhrase.Focus();
+                return;
+            }
+
+            runTime = Stopwatch.StartNew();
+            textBoxPhrase.Enabled = false;
+            buttonStart.Enabled = false;
+
+            string validatePhrase = textBoxPhrase.Text.ToUpper();
+            for (int i = 0; i < validatePhrase.Length; i++)
+            {
+                if (validatePhrase[i] != 32 && ((validatePhrase[i] < 65) || (validatePhrase[i] > 90)))
+                {
+                    _ = MessageBox.Show("The phrase can only contain characters A - Z. Please remove any special characters and try again.");
+                    _ = textBoxPhrase.Focus();
+                    runTime.Reset();
+                    return;
+                }
+            }
+
+            listBoxResults.Items.Clear();
+            FindPhrase(validatePhrase);
+            runTime.Stop();
+            labelRunTime.Text = runTime.Elapsed.ToString();
+        }
+
+        private void FindPhrase(string weaselPhrase)
+        {
+            string nextGuess;
+            List<Phrase> attemptPhrases = new();
+
+            // Initialize 100 random strings
+            attemptPhrases = NewPhrasePopulation(weaselPhrase);
+
+            do
+            {
+                // Mutate the list of strings
+                MutatePhraseList(attemptPhrases);
+
+                // Get the closest match
+                nextGuess = FuzzyMatch(attemptPhrases, weaselPhrase);
+
+                // Add the match to the listBox
+                listBoxResults.Items.Add(nextGuess);
+
+                attemptPhrases.Clear();
+
+                // New list of 100 strings
+                for (int i = 0; i < 100; i++)
+                {
+                    attemptPhrases.Add(new Phrase(nextGuess));
+                }
+
+            } while (nextGuess != weaselPhrase);
+
+        }
+
+        private List<Phrase> NewPhrasePopulation(string weaselPhrase)
+        {
+            Random random = new Random();
+            List<Phrase> newList = new List<Phrase>();
+
+            for (int count = 0; count < 100; count++)
+            {
+                char[] newString = new char[weaselPhrase.Length];
+                for (int currentLetter = 0; currentLetter < weaselPhrase.Length; currentLetter++)
+                {
+                    int newChar = random.Next(64, 91);
+                    if (newChar == 64) { newChar = 32; }
+                    newString[currentLetter] = (char)newChar;
+                }
+
+                newList.Add(new Phrase(new String(newString)));
+            }
+
+            return newList;
+        }
+
+        private string FuzzyMatch(List<Phrase> attemptPhrases, string weaselPhrase)
+        {
+            Phrase bestMatch;
+
+            foreach (var currentPhrase in attemptPhrases)
+            {
+                for (int currentLetter = 0; currentLetter < currentPhrase.PhraseString.Length; currentLetter++)
+                {
+                    if (currentPhrase.PhraseString[currentLetter] == weaselPhrase[currentLetter])
+                    {
+                        currentPhrase.Score += 1;
+                    }
+                }
+            }
+
+            bestMatch = attemptPhrases[0];
+
+            foreach (var currentPhrase in attemptPhrases)
+            {
+                if (currentPhrase.Score > bestMatch.Score)
+                { bestMatch = currentPhrase; }
+            }
+
+            return bestMatch.PhraseString;
+        }
+
+        private void MutatePhraseList(List<Phrase> attemptPhrases)
+        {
+            Random random = new();
+
+            foreach (var currentPhrase in attemptPhrases)
+            {
+                //Reset the score
+                currentPhrase.Score = 0;
+
+                for (int currentLetter = 0; currentLetter < currentPhrase.PhraseString.Length; currentLetter++)
+                {
+                    if (random.Next(100) < 5)
+                    {
+                        int newChar = random.Next(64, 91);
+                        if (newChar == 64) { newChar = 32; }
+                        currentPhrase.CharArray[currentLetter] = (char)newChar;
+                    }
+                }
+            }
+        }
+    }
+
+    internal class Phrase
+    {
+        private char[] charArray;
+
+        public string PhraseString
+        {
+            get
+            {
+                return new string(charArray);
+            }
+        }
+
+        public char[] CharArray
+        {
+            get { return charArray; }
+        }
+
+        public int Score { get; set; } = 0;
+
+        public Phrase(string phrase)
+        {
+            charArray = phrase.ToCharArray();
+        }
+    }
+}
